1647be41663680e2c54fb0eccc4dfafed1792324
AlekseyScorpi <timoshin_aleksey02@mail.ru>
2021-02-01 22:27:46 +0300

    fix

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 print("Hello, human!")
 print("I want to be nice program and give happy for people)")
 print("Ok, so, what's your name?")
-name = input();
+name = input()
 print("Nice to meet you", name)
 print("I belive that i will never a bad program!")
