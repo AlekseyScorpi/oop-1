317dcea4067b556c5f961841a502f4b75a68ad94
AlekseyScorpi <timoshin_aleksey02@mail.ru>
2021-02-01 22:14:54 +0300

    belive

--- code.py
+++ code.py
@@ -3,3 +3,4 @@ print("I want to be nice program and give happy for people)")
 print("Ok, so, what's your name?")
 name = input();
 print("Nice to meet you", name)
+print("I belive that i will never a bad program!")
