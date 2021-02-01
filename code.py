43cba92337c4ac0446deadeb4bb8ed08843b788b
AlekseyScorpi <timoshin_aleksey02@mail.ru>
2021-02-01 20:30:51 +0300

    modified code.py

--- code.py
+++ code.py
@@ -1,2 +1,5 @@
-print("Nice to meet you!")
+print("Hello, human!")
 print("I want to be nice program and give happy for people)")
+print("Ok, so, what's your name?")
+name = input();
+print("Nice to meet you", name)
